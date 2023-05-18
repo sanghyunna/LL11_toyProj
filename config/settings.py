b316734bfbcfeb446de538d58846e5096d53e0c7
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-05-18 21:39:40 +0900

    updates

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,6 @@ PROJECT_APPS = [
 
 THIRD_PARTY_APPS = [
     'corsheaders',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
@@ -82,6 +81,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
